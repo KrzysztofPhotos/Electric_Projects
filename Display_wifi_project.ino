7a1942cedc410a938ed33dd68cc5bedf91e845f3
Krzychu49346 <75449838+KrzysztofPhotos@users.noreply.github.com>
2023-06-21 09:12:32 +0200

    DZIALA WPISYWANIE ALE NIE DZIALA EEPRON

--- Display_wifi_project.ino
+++ Display_wifi_project.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <LiquidCrystal_I2C.h>
-#include <Wire.h> 
-
+#include <Wire.h>
+#include <EEPROM.h>
 
 // Ustawienia sieci Wi-Fi
 const char* ssid = "StealthNet";
@@ -12,7 +12,10 @@ const char* password = "Gt%>SB#sW:EuS@qu_5A$w";
 ESP8266WebServer server(80);
 
 // Ustawienia wyświetlacza
-LiquidCrystal_I2C lcd(0x27,20,4);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+// Adres w pamięci EEPROM, w którym przechowywany będzie tekst
+int address = 512;
 
 String text = ""; // Zmienna przechowująca wpisany tekst
 
@@ -33,26 +36,31 @@ void handleSubmit() {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(text);
+
+    // Zapisz tekst w pamięci EEPROM
+    int length = text.length() + 1; // Długość tekstu, włącznie z końcowym znakiem null
+    for (int i = 0; i < length; ++i) {
+      EEPROM.write(address + i, text[i]);
+    }
+    EEPROM.commit(); // Potwierdź zapis do pamięci EEPROM
   }
   server.send(200, "text/plain", "OK");
 }
 
 void setup() {
-
   Serial.begin(115200);
 
-  lcd.init();                      // initialize the lcd 
+  lcd.init(); // initialize the lcd
   lcd.init();
-  // Print a message to the LCD.
   lcd.backlight();
-  lcd.setCursor(3,0);
+  lcd.setCursor(3, 0);
   lcd.print("Hello, world!");
-  lcd.setCursor(2,1);
+  lcd.setCursor(2, 1);
   lcd.print("Ywrobot Arduino!");
 
   delay(5000);
 
-  lcd.begin(16, 2);
+  lcd.begin(20, 4);
   lcd.print("Arduino WiFi");
   lcd.setCursor(0, 1);
   lcd.print("Waiting for WiFi");
@@ -69,6 +77,19 @@ void setup() {
   lcd.print("Connected to WiFi");
   lcd.setCursor(0, 1);
   lcd.print("IP: " + WiFi.localIP().toString());
+
+  // Odczytaj tekst z pamięci EEPROM
+  char character;
+  int i = 0;
+  while ((character = EEPROM.read(address + i)) != '\0') {
+    text += character;
+    i++;
+  }
+
+  lcd.setCursor(0, 2);
+  lcd.print("Last text: ");
+  lcd.setCursor(0, 3);
+  lcd.print(text);
 
   server.on("/", handleRoot);
   server.on("/submit", handleSubmit);
