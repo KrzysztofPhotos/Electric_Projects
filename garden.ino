683bb9b93275c79330938d2aaf533acc1ededa72
Krzychu49346 <75449838+KrzysztofPhotos@users.noreply.github.com>
2023-07-24 20:34:17 +0200

    Update garden.ino

--- garden.ino
+++ garden.ino
@@ -7,27 +7,30 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 DHT dht(DHTPIN, DHTTYPE);
 
-const int button1Pin = 8;   // Pin podłączony do przelacznika 1
-const int button2Pin = 9;   // Pin podłączony do przelacznika 2
+const int button1Pin = 0;   // Pin podłączony do przelacznika 1
+const int button2Pin = 6;   // Pin podłączony do przelacznika 2
 const int controlPin = 10;  // Pin, który ma zmieniać stan (wysoki/niski) wlacza/wylacza LEDY
 
 int prevButton1;  // Zmienna do przechowywania poprzedniego stanu przycisku 1
 int prevButton2;  // Zmienna do przechowywania poprzedniego stanu przycisku 2
 
 void setup() {
-
+  //Serial.begin(9600);
   lcd.begin(16, 2);
   dht.begin();
   pinMode(button1Pin, INPUT);   // Przycisk 1 jako wejście
   pinMode(button2Pin, INPUT);   // Przycisk 2 jako wejście
   pinMode(controlPin, OUTPUT);  // Pin sterujący jako wyjście
 
-  pinMode(8, INPUT);
-  int currentButton1 = digitalRead(button1Pin);
-  int currentButton2 = digitalRead(button2Pin);
+
+  
+  int prevButton1 = digitalRead(button1Pin);
+  int prevButton2 = digitalRead(button2Pin);
 }
 
 void loop() {
+  //Serial.println(digitalRead(0));
+  //.println(digitalRead(1));
 
   if (digitalRead(button1Pin) != prevButton1){
     digitalWrite(controlPin, !digitalRead(controlPin));
@@ -62,17 +65,6 @@ void loop() {
     lcd.print("%");
   }
 
-  delay(500);
+  delay(200);
 
-  // if (digitalRead(8) == "HIGH"){
-  //   lcd.clear();
-  //   lcd.setCursor(0,0);
-  //   lcd.print("HIGH");
-  //   }
-  // else{
-  //   lcd.clear();
-  //   lcd.setCursor(0,0);
-  //   lcd.print(digitalRead(8));
-  //}
-  delay(500);
 }
