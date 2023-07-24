aacaf978d55d021740a1cf2df5ecc3f3e6878e67
Krzychu49346 <75449838+KrzysztofPhotos@users.noreply.github.com>
2023-07-24 21:24:38 +0200

    Update garden.ino

--- garden.ino
+++ garden.ino
@@ -8,29 +8,24 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 DHT dht(DHTPIN, DHTTYPE);
 
 const int button1Pin = 0;   // Pin podłączony do przelacznika 1
-const int button2Pin = 6;   // Pin podłączony do przelacznika 2
+const int button2Pin = 1;   // Pin podłączony do przelacznika 2
 const int controlPin = 10;  // Pin, który ma zmieniać stan (wysoki/niski) wlacza/wylacza LEDY
 
 int prevButton1;  // Zmienna do przechowywania poprzedniego stanu przycisku 1
 int prevButton2;  // Zmienna do przechowywania poprzedniego stanu przycisku 2
 
 void setup() {
-  //Serial.begin(9600);
   lcd.begin(16, 2);
   dht.begin();
   pinMode(button1Pin, INPUT);   // Przycisk 1 jako wejście
   pinMode(button2Pin, INPUT);   // Przycisk 2 jako wejście
   pinMode(controlPin, OUTPUT);  // Pin sterujący jako wyjście
-
-
   
   int prevButton1 = digitalRead(button1Pin);
   int prevButton2 = digitalRead(button2Pin);
 }
 
 void loop() {
-  //Serial.println(digitalRead(0));
-  //.println(digitalRead(1));
 
   if (digitalRead(button1Pin) != prevButton1){
     digitalWrite(controlPin, !digitalRead(controlPin));
@@ -40,9 +35,6 @@ void loop() {
     digitalWrite(controlPin, !digitalRead(controlPin));
     prevButton2 = digitalRead(button2Pin);
   }
-  
-
-
 
   // Odczytanie temperatury i wilgotności
   float temperature = dht.readTemperature();
